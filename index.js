c634e16a17e0d5e3357aaee293df97844d8f546d
Whitney Monk <whitneym92@gmail.com>
2020-10-18 10:30:24 -0500

    run node index.js to get chat working

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const app = express()
+const server = require('http').createServer(app)
+const port = process.env.PORT || 3000
+const io = require('socket.io')(server)
+const path = require('path')
+app.use(express.static(path.join(__dirname + '/public')))
+
+// just to test the server
+app.get('/', (req, res) => {
+  res.status(200).send('Working')
+})
+
+io.on('connection', socket => {
+    // console.log('Some client connected')
+  
+    socket.on('chat', message => {
+      // console.log('From client: ', message)
+      io.emit('chat', message)
+    })
+  })
+
+server.listen(port, () => {
+  console.log(`Server running on port: ${port}`)
+})
